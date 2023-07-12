ed739e1fd97aa7527fe3ce98d6f4a63dc1c7d760
ilhamydn17 <ilhamyudantyo@gmail.com>
2023-07-12 11:02:20 +0700

    fix

--- fizzbuzz-test.php
+++ fizzbuzz-test.php
@@ -1,12 +1,12 @@
 <?php
 
     for ($i=1; $i <=100 ; $i++) { 
-        if($i % 3 == 0 && $i % 5 == 0){
-            echo  $i . " FizzBuzz ";
-        }else if($i % 3 == 0){
-            echo $i. " Fizz ";
-        }else if($i % 5 == 0){
-            echo $i . " Buzz ";
+        if($i % 3 == 0){
+            echo "Fizz";
+        } else if ($i % 5 == 0) {
+            echo "Buzz";
+        }else if($i % 3 == 0 && $i % 5 == 0){
+            echo "FizzBuzz";
         }else{
             echo $i;
         }
